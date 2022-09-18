d5fc74d9f1aecdc937c7e645f51fa0bc60daaced
Natalia Volkova <Natalia.volkofff@gmail.com>
2022-09-18 23:10:37 +0200

    feat: update Home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,65 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 
-import styles from '@/pages/index.module.css'
+import styles from '@/pages/index.module.css';
+
+import { IntegerToRomanForm } from '@/components/IntegerToRomanForm';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Roman Numerals Converter</title>
       </Head>
 
       <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className={styles.title}>Roman Numerals Converter</h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          Convert numbers to Roman numerals. Use this Roman numeral converter to
+          convert numbers from 1 to 1,000 into Roman numerals.
+          <br />
+          Roman numerals are a number system developed in ancient Rome where
+          letters represent numbers. The modern use of Roman numerals involves
+          the letters I, V, X, L, C, D, and M.
         </p>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="https://vercel.com/new" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <IntegerToRomanForm />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
